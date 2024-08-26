db46959fc2cab30c6d5ed33a34ef54a6516ded40
bed-com-43-19 <bed-com-43-19@unima.ac.mw>
2024-08-26 14:22:31 +0200

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+/* ROUTES IMPORTS */
+import dashboardRoutes from "./routes/dashboardRoutes" 
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes); ///http://localhost:8000/dashboard
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
+
