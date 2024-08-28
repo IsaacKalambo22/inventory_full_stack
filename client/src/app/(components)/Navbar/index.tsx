b8c6c26a078b868fe01581ce05691b8ab1e360c5
Isaac Kalambo <kalamboisaac405@gmail.com>
2024-08-28 09:36:42 +0200

    added some images

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -4,6 +4,7 @@ import { setisDarkMode, setIsSidebarCollapsed } from '@/state'
 import { Bell, Menu, Moon, Settings, Sun } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 
 
@@ -73,7 +74,13 @@ const Navbar = () => {
           </div>
           <hr className='w-0 h-7 border border-solid border-1 border-gray-300 mx-3'/>
           <div className="flex items-center gap-3 cursor-pointer">
-            <div className="w-9 h-9">image</div>
+          {/* <Image
+              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.png"
+              alt="Profile"
+              width={50}
+              height={50}
+              className="rounded-full h-full object-cover"
+            /> */}
             <span className='font-semibold'>AIK</span>
           </div>
         </div>
